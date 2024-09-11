b79160f50606c750109d0ce7154edc220aa80d33
Stanley <stanleyemeke71@gmail.com>
2024-09-11 05:38:37 +0100

    Basic application

---
+++ 0x01-react_intro/task_0/dashboard/src/App.js
@@ -0,0 +1,23 @@
+// src/App.js
+import React from "react";
+import holbertonLogo from "./Holberton_logo.jpg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={holbertonLogo} alt="Holberton Logo" />
+        <h1>School dashboard</h1>
+      </header>
+      <body className="App-body">
+        <p>Login to access the full dashboard</p>
+      </body>
+      <footer className="App-footer">
+        <p>Copyright 2020 - Holberton School</p>
+      </footer>
+    </div>
+  );
+}
+
+export default App;
